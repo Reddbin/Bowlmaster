ba543932b469e840e8c03df59a6a35d866319fd8
Oliver Pieloth <oliver.pieloth@gmx.de>
2017-04-21 09:50:13 +0200

    After Second Test Phase
    Succesfully tested for return tidy.

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -8,6 +8,7 @@ using NUnit.Framework;
 public class ActionMasterTest {
 
 	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
 
 	[Test]
 	public void T00PassingTest(){
@@ -18,5 +19,11 @@ public class ActionMasterTest {
 	public void T01OneStrikeReturnsEndTurn(){
 		ActionMaster actionMaster = new ActionMaster();
 		Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+	}
+
+	[Test]
+	public void T02Bowl8ReturnsTidy (){
+		ActionMaster actionMaster = new ActionMaster();
+		Assert.AreEqual(tidy, actionMaster.Bowl(8));
 	}
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ActionMaster {
-
 	public enum Action {Tidy, Reset, EndTurn, EndGame};
+
+	// private int[] bowls = new int[21];
+	private int bowl = 1;
 
 	public Action Bowl (int pins){
 
 		if(pins <0 || pins >10) {throw new UnityException("Invalid pin count");}
 
-		//other Behaviour here
+		//other Behaviour here, e.g. last frame
 
 		if(pins == 10){
+			bowl += 2;
 			return Action.EndTurn;
 		}
-
-		// Other behaviour here
+			
+		if(bowl % 2 != 0) { // Mid frame (or last frame)
+			bowl +=1;
+			return Action.Tidy;
+		}
 
 		throw new UnityException("Not sure what action to return!");
 	}
